b53aa494fc62e9c1c30c51bd221ee4ffe13ced78
Andrew Yablonski <Dron2310@gmail.com>
2017-11-01 13:09:32 +0300

    Update html->text

--- p_utils.js
+++ p_utils.js
@@ -7,19 +7,79 @@ exports.clearForMultipleSpaces = function (str) {
     return str.replace(/  +/g, ' ');
 };
 
-exports.htmlToText = function (html) {
-    if (typeof html !== "string") {
-        html = html.html();
+exports.htmlToText = function (str) {
+    const DEBUG_LOGS = false;
+    //console.log('\n\n' + str);
+    if (typeof str !== "string") {
+        str = str.html();
     }
-    let text = html.replace(/<\s*((br)|(p))\s*[\/]?>/gi, '\n'); // замен <p> и <br> на перенос строки
-    text = text.replace(/<[^>]*>/g, ''); // очиствка от каких-либо html тегов
+    if (str.trim() === '') {
+        return '';
+    }
 
-    text = text.replace(/\s{1,2}(?=[^А-ЯA-Z\d\s])/gm, ''); // очистка от пробелов в слове
-    text = text.replace(/\s{2,}/gm, ' '); // замена множественных пробелов на один пробел
-    text = text.trim();
-    //console.log('[%d]\'%s\'', 1, text);
-    return text || html;
+    let text = removeTags(str);
+    const wordArr = text.match(/[\wА-я]+/gm);
+    if (wordArr !== null) {
+        // определение кол-ва пробелов
+        const spacesNumber = (str.match(/\s{1,2}/gm) || []).length;
+        const wordsNumber = wordArr.length;
+
+        let averageWordLength = 0;
+        let upCaseWordsNumber = 0;
+        let lowCaseWordsNumber = 0;
+        for (let i = 0; i < wordsNumber; i++) {
+            const word = wordArr[i];
+
+            // подсчёт суммы длинн слов
+            averageWordLength += word.length;
+
+            // определение кол-ва слов, начинающихся с верхнего/нижнего регистра
+            const firstChar = word.charAt(0);
+            if (firstChar.toUpperCase() === firstChar) {
+                // Upper case
+                upCaseWordsNumber++;
+            } else {
+                // Lower case
+                lowCaseWordsNumber++;
+            }
+        }
+        averageWordLength = averageWordLength / wordsNumber; // средняя длина слова в строке
+
+        let ulr = upCaseWordsNumber / (lowCaseWordsNumber === 0 ? 1 : lowCaseWordsNumber); // соотношение верхнего-нижнего регистра
+
+        const swr = spacesNumber / wordsNumber; // space / words number
+        if (DEBUG_LOGS) console.log('[]: %d/%d = %d; averLen= %d ulr =%d', spacesNumber, wordsNumber, swr, averageWordLength, ulr);
+
+        if (swr > 5) {
+            // если много пробелов
+            text = str.replace(/\s{1,2}(?=[^А-ЯA-Z\d\s])/gm, ''); // очистка от пробелов в слове
+        } else if (averageWordLength < 2) {
+            // если слова очень короткие
+            if (ulr > 6) {
+                // если в предложении преимущественно слова с большой буквы
+                // очистка от пробелов в слове
+                text = str.replace(/\s{1,2}(?=[^\d\s])/gm, '');
+            } else {
+                // очистка от пробелов в слове только перед слов с маленькой буквы
+                text = str.replace(/\s{1,2}(?=[^А-ЯA-Z\d\s])/gm, '');
+            }
+        }
+
+        text = removeTags(text);
+        text = text.replace(/\s{2,}/gm, ' '); // замена множественных пробелов на один пробел
+        text = text.trim();
+        if (DEBUG_LOGS) console.log('text: %s \n', text);
+
+        return text || str;
+    } else {
+        return '';
+    }
 };
+
+function removeTags(htmlString) {
+    htmlString = htmlString.replace(/<\s*((br)|(p))\s*[\/]?>/gi, '\n'); // замен <p> и <br> на перенос строки
+    return htmlString.replace(/<[^>]*>/g, ''); // очиствка от каких-либо html тегов
+}
 
 exports.getDayByIndex = function (dayIndex) {
     switch (dayIndex) {
